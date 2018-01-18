f0f4c3831dece8c127ebc61636eaf173afd7ffbd
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-01-18 09:27:11 -0500

    asdf

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -1,23 +1,35 @@
 public class ArrayMethods1
 {
 	int[] list1 = {4, 8, 3, 1, 0, 2, 6, 5, 7};
-	double[] list1 = {1.6, 1.2, 1.0, 1.3, 1.4
+	double[] list2 = {1.6, 1.2, 1.0, 1.3, 1.4, 1.7, 1.9, 1.5, 1.8, 1.1};
+	String[] list3 = {};
 	
 	public static void main(String[] args)
 	{
 		public static void insertionSort(int[] list1)
 		{
-			
+			int x = 0;
+			for(x = 0; x< list1.length; x++)
+			{
+				
+			}
 		}
 		
 		
-		public static void selectionSort(double[] list1)
+		public static void selectionSort(double[] list2)
 		{
-			
+			for (int a = 0; a < list2.length - 1; a++)
+			{
+				int min = a;
+				for (int b = a+1; b < list2.length; b++)
+				{
+					
+				}
+			}
 		}
 		
 		
-		public static void bubbleSort(String[] list1)
+		public static void bubbleSort(String[] list3)
 		{
 			
 		}
