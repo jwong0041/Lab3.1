c47fbc1e662fe8b7af084f83905970ef76b389c8
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-01-18 08:55:47 -0500

    N-nani

---
+++ Lab3.1/src/ArrayMethods1.java
@@ -0,0 +1,50 @@
+public class ArrayMethods1
+{
+	int[] list1 = {4, 8, 3, 1, 0, 2, 6, 5, 7};
+	double[] list1 = {1.6, 1.2, 1.0, 1.3, 1.4
+	
+	public static void main(String[] args)
+	{
+		public static void insertionSort(int[] list1)
+		{
+			
+		}
+		
+		
+		public static void selectionSort(double[] list1)
+		{
+			
+		}
+		
+		
+		public static void bubbleSort(String[] list1)
+		{
+			
+		}
+		
+		
+	}
+	
+	
+	public static void swap(int[] arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void swap(double[] arr, int index1, int index2)
+	{
+		double temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void swap(String[] arr, int index1, int index2)
+	{
+		String temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+
+}
