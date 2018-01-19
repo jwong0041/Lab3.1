4ca40813556e1c097949344994eb14915e3410da
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-01-19 09:27:37 -0500

    What the FFFFFFFFFFUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -1,17 +1,29 @@
 public class ArrayMethods1
 {
-	int[] list1 = {4, 8, 3, 1, 0, 2, 6, 5, 7};
-	double[] list2 = {1.6, 1.2, 1.0, 1.3, 1.4, 1.7, 1.9, 1.5, 1.8, 1.1};
-	String[] list3 = {};
+
 	
 	public static void main(String[] args)
 	{
+		
+		int[] list1 = {4, 8, 3, 1, 0, 2, 6, 5, 7, 9};
+		double[] list2 = {1.6, 1.2, 1.0, 1.3, 1.4, 1.7, 1.9, 1.5, 1.8, 1.1};
+		//String[] list3 = {};
+		insertionSort(list1);
+		System.out.println(list1);
+	}
+		
 		public static void insertionSort(int[] list1)
 		{
-			int x = 0;
-			for(x = 0; x< list1.length; x++)
+			for(int x = 1; x< list1.length; x++)
 			{
-				
+				int z = list1[x];
+				int y = x-1;
+				while(y >= 0 && list1[y] > z)
+				{
+					list1[y+1] = list1[y];
+					y = y - 1;
+				}
+				list1[y+1] = z;
 			}
 		}
 		
@@ -23,21 +35,26 @@ public class ArrayMethods1
 				int min = a;
 				for (int b = a+1; b < list2.length; b++)
 				{
-					
+					if (list2[b] < list2[min])
+					{
+						min = b;
+						swap(list2, min, b);
+					}
 				}
 			}
+			
 		}
 		
-		
+		/*
 		public static void bubbleSort(String[] list3)
 		{
 			
 		}
+		*/
+	
 		
 		
-	}
-	
-	
+		
 	public static void swap(int[] arr, int index1, int index2)
 	{
 		int temp = arr[index1];
@@ -58,5 +75,7 @@ public class ArrayMethods1
 		arr[index1] = arr[index2];
 		arr[index2] = temp;
 	}
-
+	
+	
+	
 }
